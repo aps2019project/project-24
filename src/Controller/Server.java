6cb78a418830002f48ec3a2903b69d9b16157f09
alik1378 <karimlooali@yahoo.com>
2019-05-02 11:04:10 +0430

    Merge branch 'master' of /home/ali_kmr/IdeaProjects/project24 with conflicts.

--- src/Controller/Server.java
+++ src/Controller/Server.java
@@ -1,5 +1,6 @@
 package Controller;
 ///////////////////////////////////////////////////////////ali inja comment gozashte
+
 import View.*;
 import Modules.*;
 
@@ -12,217 +13,249 @@ public class Server {
     private Game currentGame;
     private ArrayList<Game> allGames;
     private Viewer viewer;
+
     public ArrayList<Player> getAllPlayers() {
         return Player.getPlayers();
     }
-    public ArrayList<Game> getAllGames() { return allGames; }
+
+    public ArrayList<Game> getAllGames() {
+        return allGames;
+    }
+
     //================================= Account Functions ===============================//
-    public boolean userExists(String username){
+    public boolean userExists(String username) {
         ArrayList<Player> allPlayers = Player.getPlayers();
-        for(int i = 0 ; i < allPlayers.size() ; i++)
-            if(allPlayers.get(i).getUsername().equals(username))
+        for (int i = 0; i < allPlayers.size(); i++)
+            if (allPlayers.get(i).getUsername().equals(username))
                 return true;
         return false;
     }
-    public void createAccount(String username,String password){
-        new Player(username,password);
+
+    public void createAccount(String username, String password) {
+        new Player(username, password);
     }
-    public boolean isValidLogin(String username , String password){
+
+    public boolean isValidLogin(String username, String password) {
         Player player = Player.getPlayerObj(username);
-        if(player.getPassword().equals(password))
+        if (player.getPassword().equals(password))
             return true;
         return false;
     }
-    public void login(String username, String password){
+
+    public void login(String username, String password) {
         currentPlayer = Player.getPlayerObj(username);
     }
-    public void save(){}
-    public void logOut(){currentPlayer = null;}
+
+    public void save() {
+    }
+
+    public void logOut() {
+        currentPlayer = null;
+    }
 
 //================================= Shop functions ===================================//
 
-    public void searchCardInShop(String cardName){
-        for(int i = 0; i < shop.getCards().size(); i++) {
+    public void searchCardInShop(String cardName) {
+        for (int i = 0; i < shop.getCards().size(); i++) {
             if (shop.getCards().get(i).getName() == cardName) {
                 viewer.printSearchedID(shop.getCards().get(i).getCardID());
                 break;
             }
         }
     }
-    public void searchCardInCollection(String cardName){
-        for(int i = 0; i < currentPlayer.getCollection().size(); i++){
-            if(currentPlayer.getCollection().get(i).getName() == cardName)
+
+    public void searchCardInCollection(String cardName) {
+        for (int i = 0; i < currentPlayer.getCollection().size(); i++) {
+            if (currentPlayer.getCollection().get(i).getName() == cardName)
                 viewer.printSearchedID(currentPlayer.getCollection().get(i).getCardID());
         }
     }
 
-    public void buyCard(String name){
+    public void buyCard(String name) {
         shop.buy(currentPlayer, name);
     }
 
-    public void sellCardWithID(int cardID){
+    public void sellCardWithID(int cardID) {
         shop.sellWithID(currentPlayer, cardID);
     }
 
-    public void sellCardWithName(String name){
+    public void sellCardWithName(String name) {
         shop.sellWithName(currentPlayer, name);
     }
 
 
-    public ArrayList<Card> getCardsInShop(){
+    public ArrayList<Card> getCardsInShop() {
         return shop.getCards();
     }
-    public int getPlayerMoney(){
+
+    public int getPlayerMoney() {
         return currentPlayer.getMoney();
     }
-    public int getNumberOfItemsOfPlayers(){
+
+    public int getNumberOfItemsOfPlayers() {
         int counter = 0;
-        for(int i = 0; i < currentPlayer.getCollection().size(); i++){
-            if(currentPlayer.getCollection().get(i) instanceof Item)
+        for (int i = 0; i < currentPlayer.getCollection().size(); i++) {
+            if (currentPlayer.getCollection().get(i) instanceof Item)
                 counter++;
         }
         return counter;
     }
 
     //================================= Collection Functions ===============================//
-    public ArrayList<Card> cardsInCollection(){
+    public ArrayList<Card> cardsInCollection() {
         return currentPlayer.getCollection();
     }
-    public ArrayList<Integer> searchCollection(String keyword){
-        return Collection.searchCard(currentPlayer,keyword);
+
+    public ArrayList<Integer> searchCollection(String keyword) {
+        return Collection.searchCard(currentPlayer, keyword);
     }
-    public boolean isDeckExists(String keyword){
-        for(int i = 0 ; i < currentPlayer.getAllDecks().size() ; i++ )
-            if(currentPlayer.getAllDecks().get(i).getName().equals(keyword))
+
+    public boolean isDeckExists(String keyword) {
+        for (int i = 0; i < currentPlayer.getAllDecks().size(); i++)
+            if (currentPlayer.getAllDecks().get(i).getName().equals(keyword))
                 return true;
         return false;
     }
-    public void createDeckForPlayer(String keyword){
+
+    public void createDeckForPlayer(String keyword) {
         Deck d = new Deck(keyword);
         currentPlayer.getAllDecks().add(d);
     }
-    public void deleteDeckPlayer(String keyword){
-        for(int i = 0 ; i < currentPlayer.getAllDecks().size() ; i++)
-            if(currentPlayer.getAllDecks().get(i).getName().equals(keyword))
+
+    public void deleteDeckPlayer(String keyword) {
+        for (int i = 0; i < currentPlayer.getAllDecks().size(); i++)
+            if (currentPlayer.getAllDecks().get(i).getName().equals(keyword))
                 currentPlayer.getAllDecks().remove(i);
     }
-    public boolean isCardInCollection(int cardID){
-        for(int i = 0 ; i < currentPlayer.getCollection().size() ; i++)
-            if(currentPlayer.getCollection().get(i).getCardID() == cardID)
+
+    public boolean isCardInCollection(int cardID) {
+        for (int i = 0; i < currentPlayer.getCollection().size(); i++)
+            if (currentPlayer.getCollection().get(i).getCardID() == cardID)
                 return true;
         return false;
     }
-    public boolean isCardInDeck(int cardID , String keyword){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
-        for(int j = 0 ; j < d.getCards().size() ; j++)
-            if(d.getCards().get(j).getCardID() == cardID)
+
+    public boolean isCardInDeck(int cardID, String keyword) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
+        for (int j = 0; j < d.getCards().size(); j++)
+            if (d.getCards().get(j).getCardID() == cardID)
                 return true;
         return false;
     }
-    public boolean isDeckFull(String keyword){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
-        if(d.getCards().size() == 20)
+
+    public boolean isDeckFull(String keyword) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
+        if (d.getCards().size() == 20)
             return true;
         return false;
     }
-    public boolean isCardIDHero(int cardID){
-        for(int i = 0 ; i < currentPlayer.getCollection().size() ; i++)
-            if(currentPlayer.getCollection().get(i).getCardID() == cardID){
-                if( currentPlayer.getCollection().get(i) instanceof Unit && ((Unit)currentPlayer.getCollection().get(i)).isHero())
+
+    public boolean isCardIDHero(int cardID) {
+        for (int i = 0; i < currentPlayer.getCollection().size(); i++)
+            if (currentPlayer.getCollection().get(i).getCardID() == cardID) {
+                if (currentPlayer.getCollection().get(i) instanceof Unit && ((Unit) currentPlayer.getCollection().get(i)).isHero())
                     return true;
             }
         return false;
     }
-    public boolean deckHasHero(String keyword){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
-        for(int i = 0 ; i < d.getCards().size() ; i++)
-            if(d.getCards().get(i) instanceof Unit && ((Unit)d.getCards().get(i)).isHero())
+
+    public boolean deckHasHero(String keyword) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
+        for (int i = 0; i < d.getCards().size(); i++)
+            if (d.getCards().get(i) instanceof Unit && ((Unit) d.getCards().get(i)).isHero())
                 return true;
         return false;
     }
-    public void addCardToTheDeck(String keyword , int cardID){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
-        Card c = Collection.searchCard(currentPlayer,cardID);
+
+    public void addCardToTheDeck(String keyword, int cardID) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
+        Card c = Collection.searchCard(currentPlayer, cardID);
         d.getCards().add(c);
     }
-    public void deleteCardFromDeck(String keyword , int cardID){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
-        Card c = Collection.searchCard(currentPlayer,cardID);
+
+    public void deleteCardFromDeck(String keyword, int cardID) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
+        Card c = Collection.searchCard(currentPlayer, cardID);
         d.getCards().remove(c);
     }
-    public boolean isDeckValid(String keyword){
-        if(deckHasHero(keyword) && isDeckFull(keyword))
+
+    public boolean isDeckValid(String keyword) {
+        if (deckHasHero(keyword) && isDeckFull(keyword))
             return true;
         return false;
     }
-    public void setMainDeck(String keyword){
-        Deck d = Collection.searchDeck(currentPlayer,keyword);
+
+    public void setMainDeck(String keyword) {
+        Deck d = Collection.searchDeck(currentPlayer, keyword);
         currentPlayer.setMainDeck(d);
         currentPlayer.getAllDecks().remove(d);
     }
-    public Deck getDeck(String keyword){
-        return Collection.searchDeck(currentPlayer,keyword);
+
+    public Deck getDeck(String keyword) {
+        return Collection.searchDeck(currentPlayer, keyword);
     }
-    public ArrayList<Deck> getPlayerDecks(){
+
+    public ArrayList<Deck> getPlayerDecks() {
         return currentPlayer.getAllDecks();
     }
-    public Deck getPlayerMainDeck(){
+
+    public Deck getPlayerMainDeck() {
         return currentPlayer.getMainDeck();
     }
 
     //================================= Battle Functions ===============================//
-    public String showPlayersMana(){
+    public String showPlayersMana() {
         String ans = "";
         ans = ans.concat(currentGame.getPlayersOfGame()[0].getUsername() + " " + currentGame.getManaOfPlayers()[0] + " - ");
         ans = ans.concat(currentGame.getPlayersOfGame()[1].getUsername() + " " + currentGame.getManaOfPlayers()[1]);
         return ans;
     }
-    public String showGameModeInfo(){
+
+    public String showGameModeInfo() {
         String ans = "Game Mode => ";
-        if(currentGame.getGameMode().equals("heroMode")){
-            ans = ans.concat("Hero Mode\n1. Health Of Hero Player " + currentGame.getPlayersOfGame()[0].getUsername() + " : " + ((Unit)currentGame.getPlayerHero(0)).getHP() + "\n");
-            ans = ans.concat("2. Health Of Hero Player " + currentGame.getPlayersOfGame()[1].getUsername() + " : " + ((Unit)currentGame.getPlayerHero(1)).getHP() + "\n");
-        }
-        else if(currentGame.getGameMode().equals("flagHolding")){
+        if (currentGame.getGameMode().equals("heroMode")) {
+            ans = ans.concat("Hero Mode\n1. Health Of Hero Player " + currentGame.getPlayersOfGame()[0].getUsername() + " : " + ((Unit) currentGame.getPlayerHero(0)).getHP() + "\n");
+            ans = ans.concat("2. Health Of Hero Player " + currentGame.getPlayersOfGame()[1].getUsername() + " : " + ((Unit) currentGame.getPlayerHero(1)).getHP() + "\n");
+        } else if (currentGame.getGameMode().equals("flagHolding")) {
             int[] coord = new int[2];
             coord[0] = currentGame.getFlagHoldedCoordination()[0];
             coord[1] = currentGame.getFlagHoldedCoordination()[1];
             ans = ans.concat("Flag Holding Mode\nFlag Coordination = X : " + coord[0] + " , Y : " + coord[1] + "\n");
-            if(currentGame.getFlagOwner(coord[0],coord[1]) == null)
+            if (currentGame.getFlagOwner(coord[0], coord[1]) == null)
                 ans = ans.concat("Nobody has the flag !!!");
             else
-                ans = ans.concat("Player " + currentGame.getFlagOwner(coord[0],coord[1]).getUsername() + " Has The Flag !");
-        }
-        else{
+                ans = ans.concat("Player " + currentGame.getFlagOwner(coord[0], coord[1]).getUsername() + " Has The Flag !");
+        } else {
             ArrayList<String> flagsInfo = currentGame.getFlagsInfo();
             ans = ans.concat("Flags Collecting Mode\n Soldiers That have The flag :\n");
-            for(int i = 0 ; i < flagsInfo.size() ; i++ )
-                ans = ans.concat(i+1 + ". " + flagsInfo.get(i) + "\n");
+            for (int i = 0; i < flagsInfo.size(); i++)
+                ans = ans.concat(i + 1 + ". " + flagsInfo.get(i) + "\n");
         }
         return ans;
     }
-    public ArrayList<String> getPlayerUnitsInfo(String turn){
+
+    public ArrayList<String> getPlayerUnitsInfo(String turn) {
         return currentGame.getPlayerUnitsInfo(turn);
     }
-    public String showCardInfo(int cardID){
+
+    public String showCardInfo(int cardID) {
         Card card = currentGame.findCardByID(cardID);
-        if(card == null)
+        if (card == null)
             return "This Card ID is invalid !!!";
-        else if(card instanceof Unit && ((Unit)card).isHero()){
+        else if (card instanceof Unit && ((Unit) card).isHero()) {
             String ans = "";
             ans = ans.concat("Hero :\nName : " + card.getName() + "\nCost : " + ((Unit) card).getManaCost() + "\nDesc : " + ((Unit) card).getDescription());
             return ans;
-        }
-        else if(card instanceof Unit){
+        } else if (card instanceof Unit) {
             String ans = "";
             String canCombo;
-            if(((Unit) card).getCanCombo())
+            if (((Unit) card).getCanCombo())
                 canCombo = "Yes";
             else
                 canCombo = "No";
             ans = ans.concat("Minion :\nName : " + card.getName() + "\nHP : " + ((Unit) card).getHP() + " , AP : " + ((Unit) card).getAttackPower()
                     + " , MP : " + ((Unit) card).getManaCost() + "\nRange : " + ((Unit) card).getRange() + "\nCombo-Ability : "
-                    + canCombo + "\nCost : " + ((Unit) card).getManaCost() + "\nDesc : " + ((Unit) card).getDescription() );
+                    + canCombo + "\nCost : " + ((Unit) card).getManaCost() + "\nDesc : " + ((Unit) card).getDescription());
             return ans;
         }
         String ans = "";
@@ -231,26 +264,30 @@ public class Server {
     }
 
 
-    public ArrayList<String> getAllMinionsInfo(String whom){
+    public ArrayList<String> getAllMinionsInfo(String whom) {
         ArrayList<String> ans = new ArrayList<>();
 
 
         return ans;
     }
 
-    public Card findCard(int cardID){
-        return this.currentGame.findCardByID(cardID);
-    }
-
-    public boolean selectCard(int cardID){
-        if ( this.currentGame.findCardByID(cardID) != null )
-            return true;
-        else
+    public boolean selectCard(int cardID) {
+        if (this.currentGame.findCardByID(cardID) == null)
             return false;
+        else {
+            for (Cell[] cells : this.currentGame.getMap())
+                for (Cell cell : cells)
+                    if (cell.getCard().getCardID() == cardID && this.currentGame.getCardOwner(cell.getCard()).equals
+                            (this.currentGame.getPlayersOfGame()[this.currentGame.getTurn()])) {
+                        this.currentGame.setCurrentCard(cell.getCard());
+                        return true;
+                    }
+        }
+        return false;
     }
 
-    public int moveCurrentCard(int x, int y){
-        if ( this.currentGame.getCurrentCard() == null || !(this.currentGame.getCurrentCard() instanceof Unit ) )
+    public int moveCurrentCard(int x, int y) {
+        if (this.currentGame.getCurrentCard() == null || !(this.currentGame.getCurrentCard() instanceof Unit))
             return 4;
         return this.currentGame.moveCurrentCard(x, y);
     }
@@ -300,36 +337,37 @@ public class Server {
     }
     //////////////////////////// END ARMAN ////////////////////////////////
 
-    public int useSpecialPower(int x, int y){
-        if ( this.currentGame.getCurrentCard() == null || !(this.currentGame.getCurrentCard() instanceof Unit) )
+    public int useSpecialPower(int x, int y) {
+        if (this.currentGame.getCurrentCard() == null || !(this.currentGame.getCurrentCard() instanceof Unit))
             return 4;
         return this.currentGame.useSpecialPower(x, y);
     }
 
-    public ArrayList<Card> showHand(){
+    public ArrayList<Card> showHand() {
         ArrayList<Card> hand = currentGame.getHandsOfPlayers()[currentGame.getTurn()];
         return hand;
     }
-    public Card getNextCard(){
+
+    public Card getNextCard() {
         Card card = currentGame.getDecksOfPLayers()[currentGame.getTurn()].get(0);
         return card;
     }
 
-    public String insertUnit(String cardName, int x, int y){
+    public String insertUnit(String cardName, int x, int y) {
         Card card = currentGame.findCardByName(cardName);
-        if(card == null)
+        if (card == null)
             return "Invalid card name";
-        else if(!currentGame.cardIsInHand(card))
+        else if (!currentGame.cardIsInHand(card))
             return "Invalid card name";
-        else if(x > 4 || y > 8)
+        else if (x > 4 || y > 8)
             return "Invalid target";
-        else if(currentGame.getMap()[x][y].getCard() != null)
+        else if (currentGame.getMap()[x][y].getCard() != null)
             return "Invalid target";
-        else if(!currentGame.isNearHero(x, y))
+        else if (!currentGame.isNearHero(x, y))
             return "Invalid target";/////////////////////////unit o faghat mishe nazdik hero gozasht
-        else if(currentGame.getManaOfPlayers()[currentGame.getTurn()] < ((Unit)card).getManaCost())
-            return"You dont have enough mana";
-        else{
+        else if (currentGame.getManaOfPlayers()[currentGame.getTurn()] < ((Unit) card).getManaCost())
+            return "You dont have enough mana";
+        else {
             currentGame.getMap()[x][y].setCard(card);
             currentGame.getHandsOfPlayers()[currentGame.getTurn()].remove(card);
             return "Insert successfull";
@@ -337,39 +375,39 @@ public class Server {
 
     }
 
-    public int insertSpell(String cardName, int x, int y){
+    public int insertSpell(String cardName, int x, int y) {
 
     }
 
-    public void endTurn(){
+    public void endTurn() {
 
     }
 
-    public ArrayList<Item> ShowCollectables(){
+    public ArrayList<Item> ShowCollectables() {
 
     }
 
-    public int selectItem(int itemID){
+    public int selectItem(int itemID) {
 
     }
 
-    public Item showCurrentItemInfo(){
+    public Item showCurrentItemInfo() {
 
     }
 
-    public boolean useCurrentItem(int x, int y){
+    public boolean useCurrentItem(int x, int y) {
 
     }
 
-    public void showNextCard(){
+    public void showNextCard() {
 
     }
 
-    public boolean enterGraveYard(){
+    public boolean enterGraveYard() {
 
     }
 
-    public void endGame(){
+    public void endGame() {
 
     }
 
